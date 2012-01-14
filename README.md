33ae5487f79cc43acfe2b5bc8c71467a11a49c13
MOZGIII <mike-n@narod.ru>
2012-01-15 02:02:01 +0400

    Did a lot of things

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
 gmd
 ===
 
+
 What is it?
 -----------
 
 gmd is a small command-line utility to easily generate fancy HTML from Markdown.
+
+
+Installation
+------------
+
+    $ [sudo] gem install gmd
+
+
+Usage
+-----
+
+    $ gmd text.md
+
+Will generate HTML from the `text.md` and save it to `text.html`.
+
+    $ gmd text.md -l <layout>
+
+Will generate and save HTML from the `text.md` using specified layout.
+
+Built-in layouts:
+
+  + *default*
+  + *github* - something similar to new GitHub style
+  + *metal* - something like older GitHub style
+
+Actually, the deafult style is also (very very old) GitHub style ;)
--- lib/gmd.rb
+++ lib/gmd.rb
@@ -9,17 +9,26 @@ module Gmd
     def get_layout_paths(file)
       [
         file,
+        "#{file}.html.erb",
+        "#{file}/layout.html.erb",
         "layout/#{file}",
+        "layout/#{file}.html.erb",
         "layouts/#{file}",
+        "layouts/#{file}.html.erb",
+        "layouts/#{file}/layout.html.erb",
         "~/.gmd/#{file}",
-        File.join(defalut_layouts_dir, file),
+        "~/.gmd/#{file}.html.erb",
+        "~/.gmd/#{file}/layout.html.erb",
+        File.join(default_layouts_dir, file),
+        File.join(default_layouts_dir, file + ".html.erb"),
+        File.join(default_layouts_dir, file + "/layout.html.erb"),
         "~/.tilt/#{file}",
         "/etc/tilt/#{file}"
       ]
     end
 
     def choose_file_from_paths(paths)
-      paths.map { |p| File.expand_path(p) }.find { |p| File.exist?(p) }
+      paths.map { |p| File.expand_path(p) }.find { |p| File.file?(p) }
     end
 
     def default_layouts_dir
@@ -31,7 +40,13 @@ module Gmd
     end
 
     def default_layout
-      default_layouts_dir + "/default.html.erb"
+      default_layouts_dir + "/default/layout.html.erb"
+    end
+
+    def layout_meta_tags
+      <<-METATAGS
+        <meta name="generator" content="gmd #{Gmd::VERSION}">
+      METATAGS
     end
 
   end
